a4d8bd1574c1d1436067f3724cbe961c90ed0440
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-04-02 13:55:14 -0300

    Feito

--- Aprofundando/(4) - Manipulacao_String_Number/main.js
+++ Aprofundando/(4) - Manipulacao_String_Number/main.js
@@ -1,23 +1,28 @@
-    //var nome = prompt("Qual o seu nome?")
+    var nome = prompt("Qual o seu nome?")
 
     //var idade = prompt("Digite sua idade")
-    
+
     //idade = Number(idade)
 
     var preco = prompt("Quanto você pagou no seu PC?") // Str: 8500.9
 
-    preco = preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // R$ 8.500,90
-    
-    document.write(`Seu PC custou: ` +preco)
-    
-    //Number(preco) // Number: 8500.9
-     // preco = preco.toFixed(2).replace('.',',')// Number: 8500,90
+  // NODE -> //preco = preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // R$ 8.500,90
 
-    //document.write(`Olá, <strong>${nome}</strong>. Seu nome tem ${nome.length} letras` +'<br>')
+    Number(preco) // Number: 8500.9
+
+    preco = preco.toFixed(2).replace('.',',') // Number: 8500,90
+
     
-    //document.write(`Nome em maiúsculo: ` + nome.toUpperCase() +'<br>')
+    document.write(`Seu PC custou: R$ ` +preco)
+
     
-    //document.write(`Nome em minúsculo: ` + nome.toLowerCase() +'<br>')
+
+    document.write(`Olá, <strong>${nome}</strong>. Seu nome tem ${nome.length} letras` +'<br>')
+
+    
+    document.write(`Nome em maiúsculo: ` + nome.toUpperCase() +'<br>')
+    
+    document.write(`Nome em minúsculo: ` + nome.toLowerCase() +'<br>')
     
 
   
