d149379e4e7ac9732f19da280dc2fc11682f4d57
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-04-20 17:06:49 -0300

    Finalizado com anotações

--- Aprofundando/(4) - Manipulacao_String_Number/main.js
+++ Aprofundando/(4) - Manipulacao_String_Number/main.js
@@ -1,28 +1,22 @@
     var nome = prompt("Qual o seu nome?")
 
-    //var idade = prompt("Digite sua idade")
+    document.write(`Olá, <strong>${nome}</strong>. Seu nome tem ${nome.length} letras` +'<br>')
 
-    //idade = Number(idade)
+    document.write(`Nome em maiúsculo: ` + nome.toUpperCase() +'<br>')
+    document.write(`Nome em minúsculo: ` + nome.toLowerCase() +'<br>')
+
+
+    var idade = prompt("Digite sua idade")
+
+    idade = Number(idade)
 
     var preco = prompt("Quanto você pagou no seu PC?") // Str: 8500.9
-
-  // NODE -> //preco = preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // R$ 8.500,90
 
     Number(preco) // Number: 8500.9
 
     preco = preco.toFixed(2).replace('.',',') // Number: 8500,90
-
     
     document.write(`Seu PC custou: R$ ` +preco)
-
     
-
-    document.write(`Olá, <strong>${nome}</strong>. Seu nome tem ${nome.length} letras` +'<br>')
-
+    // NODE -> //preco = preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // R$ 8.500,90
     
-    document.write(`Nome em maiúsculo: ` + nome.toUpperCase() +'<br>')
-    
-    document.write(`Nome em minúsculo: ` + nome.toLowerCase() +'<br>')
-    
-
-  
