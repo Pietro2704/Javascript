3e0213da1ebc895001b5c829c0db885ddf9135b6
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-05-03 15:34:34 -0300

    Operadores

--- Aprofundando/(5) - Operadores/main.js
+++ Aprofundando/(5) - Operadores/main.js
@@ -22,4 +22,35 @@ var y = 2
 
 // Atribuição:
 
-alert(`X valia ${x}, mas agora vale ${x += x} `)
+// alert(`X = ${x} e Y = ${y}, ${x} += ${y}, vale ${x += y} `) // Atribuição de adição	
+// alert(`X = ${x} e Y = ${y}, ${x} -= ${y}, vale ${x -= y} `) // Atribuição de subtração	
+// alert(`X = ${x} e Y = ${y}, ${x} *= ${y}, vale ${x *= y} `) // Atribuição de multiplicação	
+// alert(`X = ${x} e Y = ${y}, ${x} /= ${y}, vale ${x /= y} `) // Atribuição de divisão	
+// alert(`X = ${x} e Y = ${y}, ${x} %= ${y}, vale ${x %= y} `) // Atribuição de resto	
+// alert(`X = ${x} e Y = ${y}, ${x} **= ${y}, vale ${x **= y} `) // Atribuição exponencial	
+
+//  --------------------------------------------------------------------------------------------------
+
+
+// Comparação / Relacionais:
+
+var X = '5'
+var Y = '2'
+
+// alert(`Será que ${x} é igual à ${y}? ${x == y} `)                    // igual
+// alert(`Será que ${x} é diferente de ${X}? ${x != X} `)               // diferente
+// alert(`Será que ${x} é estritamente igual à ${X}? ${x === X} `)      // estritamente igual
+// alert(`Será que ${Y} é estritamente diferente de ${y}? ${Y !== y} `) // estritamente diferente
+// alert(`Será que ${x} é maior que ${y}? ${x > y} `)                   // maior que
+// alert(`Será que ${x} é menor que ${y}? ${x < y} `)                   // menor que
+// alert(`Será que ${x} é maior ou igual que ${y}? ${x >= y} `)         // maior ou igual
+// alert(`Será que ${x} é menor ou igual que ${y}? ${x <= y} `)         // menor ou igual
+
+//  --------------------------------------------------------------------------------------------------
+
+
+// Lógicos:
+
+// alert(`Será que ${x} é igual à ${X}, e ${y} é igual à ${Y}? ${x == X && y == Y}`)  // AND
+// alert(`Será que ${x} é igual à ${y}, ou ${y} é igual à ${Y}? ${x == X || y == Y}`) // OR
+// alert(`Será que ${x} é igual à ${y}? ${!(x == y)}`)                                // NOT
