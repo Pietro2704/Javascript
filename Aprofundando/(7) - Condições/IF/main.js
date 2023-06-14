095eeefec85f26eb709dbadb5e66c44d9886abb1
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-14 12:07:31 -0300

    Apenas IF

---
+++ Aprofundando/(7) - Condições/IF/main.js
@@ -0,0 +1,11 @@
+var vel = 70
+
+console.log(`A velocidade do carro está em ${vel} KM/H`);
+
+if (vel<80){
+  console.log("O carro está no limite da via!!!")
+}
+
+vel  = 120;
+
+console.log(`A velocidade do carro está em ${vel} KM/H`);
