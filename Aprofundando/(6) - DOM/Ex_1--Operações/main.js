1733a16b362c0678ff60fe925321d71df612b31a
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-14 10:11:15 -0300

    Feito!

--- Aprofundando/(6) - DOM/Ex_1--Operações/main.js
+++ Aprofundando/(6) - DOM/Ex_1--Operações/main.js
@@ -1,1 +1,36 @@
-var n1 = document.querySelector("")
+
+var mais = document.querySelector("#mais")
+var menos = document.querySelector("#menos")
+var vezes = document.querySelector("#vezes")
+var dividido = document.querySelector("#dividido")
+
+var resultado = document.querySelector("#result")
+
+mais.addEventListener("click", Somar)
+menos.addEventListener("click", Subtrair)
+vezes.addEventListener("click", Multiplicar)
+dividido.addEventListener("click", Dividir)
+
+function Somar(){
+  var n1 = Number(document.querySelector("#num1").value)
+  var n2 = Number(document.querySelector("#num2").value)
+  resultado.value = n1 + n2;
+}
+
+function Subtrair(){
+  var n1 = Number(document.querySelector("#num1").value)
+  var n2 = Number(document.querySelector("#num2").value)
+  resultado.value = n1 - n2;
+}
+
+function Multiplicar(){
+  var n1 = Number(document.querySelector("#num1").value)
+  var n2 = Number(document.querySelector("#num2").value)
+  resultado.value = n1 * n2;
+}
+
+function Dividir(){
+  var n1 = Number(document.querySelector("#num1").value)
+  var n2 = Number(document.querySelector("#num2").value)
+  resultado.value = n1 / n2;
+}
