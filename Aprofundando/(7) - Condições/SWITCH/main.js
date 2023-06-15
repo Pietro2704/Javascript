7b3e2c2d1fd2296be3afbb877ad0d22b3a4ac0bc
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-15 10:20:00 -0300

    switch

--- Aprofundando/(7) - Condições/SWITCH/main.js
+++ Aprofundando/(7) - Condições/SWITCH/main.js
@@ -7,27 +7,27 @@ else if (limit_via % 2 == 0){
 
   switch(limit_via){
     case 30:
-      console.log(`É uma via local`)
+      console.log(`Esta é uma via local`)
       break
 
     case 40:
-      console.log(`É uma via coletora`)
+      console.log(`Esta é uma via coletora`)
       break
 
     case 60:
-      console.log(`É uma via arterial`)
+      console.log(`Esta é uma via arterial`)
       break
 
     case 80:
-      console.log(`É uma via de transito rápido`)
+      console.log(`Esta é uma via de transito rápido`)
       break
     
     default:
-      console.log(`É uma rodovia`)
+      console.log(`Esta é uma rodovia`)
       break
   }
 
 }
 else{
-  console.log(`Não é a velocidade do seu carro, mas o limite da via`)
+  console.log(`Tem uma placa na via sinalizando que a velocidade máxima da via é de ${limit_via} KM/H`)
 }
