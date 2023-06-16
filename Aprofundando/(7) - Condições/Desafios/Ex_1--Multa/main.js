1caa6ed9b53f1c16241ccac22c6d41086ab02704
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-16 13:13:18 -0300

    sistema de multa JS

--- Aprofundando/(7) - Condições/Desafios/Ex_1--Multa/main.js
+++ Aprofundando/(7) - Condições/Desafios/Ex_1--Multa/main.js
@@ -3,12 +3,17 @@ var verificar = document.querySelector('#verifica')
 verificar.addEventListener("click", Calcular)
 
 function Calcular(){
+
   var vel = (document.querySelector("#vel")).value
   vel = Number(vel)
   
   var res = document.querySelector("#res")
+  res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}</strong> km/h</p>`
+  res.innerHTML+=`<p>Ainda está no limite permido da via</p>`
 
   if(vel>60){
-    res.innerHTML = "<p></p>"
+    res.innerHTML = `<p>Você foi <strong>MULTADO</strong> por excesso de velocidade</p>`
+  
   }
+  
 }
