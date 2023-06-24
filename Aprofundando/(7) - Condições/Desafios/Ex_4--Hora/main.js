847a3cdf30ed80c50c5f6b329fc2301baadb4264
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-24 16:23:56 -0300

    Aprendi classe Date()

--- Aprofundando/(7) - Condições/Desafios/Ex_4--Hora/main.js
+++ Aprofundando/(7) - Condições/Desafios/Ex_4--Hora/main.js
@@ -2,12 +2,12 @@ var btn = document.querySelector("#confirm");
 btn.addEventListener("click", diasVividos);
 
 function diasVividos() {
-  var agora = new Date();
-
+  var hoje = new Date();
+  
 /*  
-var diaAgora = agora.getDate();
-var mesAgora = agora.getMonth();
-var anoAgora = agora.getFullYear();
+var diaAgora = hoje.getDate();
+var mesAgora = hoje.getMonth();
+var anoAgora = hoje.getFullYear();
 */
 
   var nascimento = document.querySelector("#nascimento").value;
@@ -20,7 +20,7 @@ var anoNascimento = dataNascimento.getFullYear();
 */
 
 // a variável 'diferenca' conterá a diferença em milissegundos entre as duas datas.
-  var diferenca = agora - dataNascimento; 
+  var diferenca = hoje - dataNascimento; 
 
 // Existem 1000 milissegundos em um segundo.
 // Existem 60 segundos em um minuto.
@@ -29,6 +29,6 @@ var anoNascimento = dataNascimento.getFullYear();
   var diasVividos = Math.floor(diferenca / (1000 * 60 * 60 * 24));
 
   var result = document.querySelector("#res");
-  result.innerHTML = `Você viveu ${diasVividos} dias até agora.`;
+  result.innerHTML = `Você viveu ${diasVividos} dias até hoje.`;
 
 }
