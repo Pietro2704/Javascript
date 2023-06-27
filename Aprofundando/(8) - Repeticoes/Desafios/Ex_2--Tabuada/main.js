56a88c9ba3ce4d7caf3cb4daf73035e871fe3e1c
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-06-27 15:03:28 -0300

    Tabuada

--- Aprofundando/(8) - Repeticoes/Desafios/Ex_2--Tabuada/main.js
+++ Aprofundando/(8) - Repeticoes/Desafios/Ex_2--Tabuada/main.js
@@ -3,17 +3,21 @@ btn.addEventListener("click",Verificar);
 
 function Verificar(){
   var tabuada = parseInt(document.querySelector("#numero").value);
+  
   if(isNaN(tabuada)){
     alert("[ERRO] Verifique os dados e tente novamente");
   }
   else{
+
     var msg = document.querySelector("#msg");
     msg.innerHTML = `Tabuada do ${tabuada}:`
 
     var acontece = document.querySelector("#acontece");
-    var txtarea = document.createElement("textarea");
-    txtarea.style.height = "165px";
     acontece.innerHTML="";
+
+    var txtarea = document.createElement("textarea");
+    txtarea.disabled = true;
+    txtarea.style.height = "165px";
 
     for(var i=0; i<=10; i++){
       res = tabuada*i;
