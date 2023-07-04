e5bf6bc38cf5b78c2bf2d14b2dbc4e2ffe0f2fcf
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-07-04 15:25:01 -0300

    Add função fantasma

--- Curso/(10) - Funcao/Desafios/Ex_1--Analisador_Numero/main.js
+++ Curso/(10) - Funcao/Desafios/Ex_1--Analisador_Numero/main.js
@@ -43,6 +43,38 @@ function CalculaMedia(vetor){
   return media.toFixed(2);
 }
 
+/*
+function DescobrirInfo(vetor){
+  
+  var tamanho = vetor.length;
+  var maior = vetor[0];
+  var menor = vetor[0];
+  var soma = 0;
+
+  for(let pos in vetor){
+    soma += vetor[pos];
+
+    if(vetor[pos]>maior){
+      maior = vetor[pos];
+    }
+    else if(vetor[pos]<menor){
+      menor = vetor[pos];
+    }
+  }
+
+  var media = soma / vetor.length;
+
+  return {
+  tamanho: tamanho,
+  maior: maior,
+  menor: menor,
+  soma: soma,
+  media: media.toFixed(2),
+  };
+
+}
+*/
+
 var vetor = [];
 
 var add = document.querySelector("#add");
@@ -62,6 +94,8 @@ function Add(){
     else{
       vetor.push(num);
       txtarea.textContent = `${vetor.join("\n")}`
+      document.querySelector("#num").value = '';
+      document.querySelector("#num").focus();
     }
   }
 }
@@ -71,17 +105,21 @@ var btn = document.querySelector("#fim");
 btn.addEventListener("click",Verificar);
 
 function Verificar(){
-  var msg = document.querySelector("#msg");
+  if(vetor.length == 0){
+    alert("[ERRO] Adicione números!")
+  }else{
+    var msg = document.querySelector("#msg");
+    
+    var tamanho = vetor.length;
+    var maior = VerMaior(vetor);
+    var menor = VerMenor(vetor);
+    var soma = SomaValores(vetor);
+    var media = CalculaMedia(vetor);
   
-  var tamanho = vetor.length;
-  var maior = VerMaior(vetor);
-  var menor = VerMenor(vetor);
-  var soma = SomaValores(vetor);
-  var media = CalculaMedia(vetor);
-
-  msg.innerHTML = `Tamanho do vetor: ${tamanho}<br>`;
-  msg.innerHTML += `Maior valor: ${maior}<br>`;
-  msg.innerHTML += `Menor valor: ${menor}<br>`;
-  msg.innerHTML += `Soma de todos os valores: ${soma}<br>`;
-  msg.innerHTML += `Média dos valores: ${media}<br>`;
+    msg.innerHTML = `Tamanho do vetor: ${tamanho}<br>`;
+    msg.innerHTML += `Maior valor: ${maior}<br>`;
+    msg.innerHTML += `Menor valor: ${menor}<br>`;
+    msg.innerHTML += `Soma de todos os valores: ${soma}<br>`;
+    msg.innerHTML += `Média dos valores: ${media}<br>`;
+  }
 }
